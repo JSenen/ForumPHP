2c5108c22a225f99774340b67206238111d0255f
Juan Senen <juansenen@hotmail.com>
2023-03-17 23:12:23 +0100

    List Post ready

--- model/listpost_model.php
+++ model/listpost_model.php
@@ -1,32 +1,34 @@
 <?php
-function listTopics($dbh,$id){
+function listPost($dbh,$id){
   //Recibimos la conexión desde el controller
   try {
       //configuramos la consulta a la base de datos
-      $stmt = $dbh->prepare("SELECT topic_id, topic_subject, topic_by FROM topics WHERE topic_cat = '$id'");
+      $stmt = $dbh->prepare("SELECT post_id, post_content, post_date FROM posts WHERE post_topic = '$id'");
       $stmt->execute();
   
       $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-      //Visualizamos el listado de categorias
+      //Visualizamos el listado de comentarios
       ?>        
       <main class="container" id="content">
         <section class="header">
-          <h1>Temas</h1>
+          <h1>Comentarios</h1>
         </section>
 
-        <table class="categories">
+        <table>
           <tr>
-            <th>Temas</th>
+            <th>Comentarios</th>
+            <th>Fecha</th>
             <th class="edit">Edit</th>
             <th class="del">Delete</th>
           </tr>
-          <?php foreach ($resultado as $topic) { ?>
+          <?php foreach ($resultado as $post) { ?>
             <tr>
-              <td><a href="indexPost.php?id=<?= $topic['topic_id'] ?>"><strong><?= $topic['topic_subject'] ?></strong></td>
-              <td><a href="#<?= $topic['topic_id'] ?>"   
+              <td><?= $post['post_content'] ?></td>
+              <td style="font-size:75%;"><?= $post['post_date'] ?></td>
+              <td><a href="#<?= $post['post_id'] ?>"   
                     class="btn btn-primary">Edit</a></td>
-              <td><a href="#<?= $topic['topic_id'] ?>"
+              <td><a href="#<?= $post['post_id'] ?>"
                     class="btn btn-danger">Delete</a></td>
             </tr>
           <?php } ?>
---
+++ view/post_view.php
@@ -0,0 +1,3 @@
+<?php
+include 'header_view.php';    //Llamamos a la cabecera de la página
+?>
