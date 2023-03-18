054facf6c59fd779fb44bf45588345632024c85f
Juan Senen <juansenen@hotmail.com>
2023-03-18 13:09:09 +0100

    Usuario login funcionando

--- controller/login_controller.php
+++ controller/login_controller.php
@@ -1,8 +1,7 @@
 <?php
 require('./domain/Conecction.php'); //Clase conexion
 function checkLogin(){
-  $conection = new Conecction(); //Objeto conexion
-  $dbh = $conection->getConection(); //Conectamos a la base de datos
+  
   require('./view/login_view.php');
   require('./model/login_model.php');
   getLogin();
