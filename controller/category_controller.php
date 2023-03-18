b47032947adce9840afe940fecd9b020dfa7887c
Juan Senen <juansenen@hotmail.com>
2023-03-18 11:37:25 +0100

    Update listcategory_model

--- controller/category_controller.php
+++ controller/category_controller.php
@@ -1,5 +1,6 @@
 <?php
 require('./domain/Conecction.php');   //Requerimos la clase Connection
+require('./domain/Category.php');   //Requerimos la clase Categoria
 //Controlador de diferentes funciones de la página
 function iniForum(){                      
   $conection = new Conecction();          //Creamos un objeto conexion
@@ -10,8 +11,8 @@ function iniForum(){
 function iniCategory(){
   $conection = new Conecction();            //Creamos objeto conexion
   $dbh = $conection->getConection();        //Realizamos la conexion y la almacenamos en una varible
-  include('view/list_view.php');            //Llamamos a la vista del listado de categorias
+  //include('view/list_view.php');            //Llamamos a la vista del listado de categorias
   include('model/listcategory_model.php');  //LLamamos al modelo que gestiona el listado de categorias
   listCategory($dbh);                       //Llamada a la funcion listar categorias del modelo anterior y pasamos conexión
-  
+  include ('./view/list_view.php');
 }
