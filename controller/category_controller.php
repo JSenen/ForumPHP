5b4218db221fdaa6861a36a6889c3cf7e08c9a18
Juan Senen <juansenen@hotmail.com>
2023-03-18 11:03:10 +0100

    Extraida Conexion como clase

--- controller/category_controller.php
+++ controller/category_controller.php
@@ -1,14 +1,15 @@
 <?php
+require('./domain/Conecction.php');   //Requerimos la clase Connection
 //Controlador de diferentes funciones de la página
-function iniForum(){                      //Función inicio de la página
-  require('model/connect_model.php');
-  getConection();  
+function iniForum(){                      
+  $conection = new Conecction();          //Creamos un objeto conexion
+  $conection->getConection();             //Abrimos conexion
   include('view/home_view.php');
 }
-
+//Funcion para el inicio de la página Categorias
 function iniCategory(){
-  require('model/connect_model.php');       //Llamamos al modelo de conexion PDO
-  $dbh = getConection();                    //Realizamos la conexion y la almacenamos en una varible
+  $conection = new Conecction();            //Creamos objeto conexion
+  $dbh = $conection->getConection();        //Realizamos la conexion y la almacenamos en una varible
   include('view/list_view.php');            //Llamamos a la vista del listado de categorias
   include('model/listcategory_model.php');  //LLamamos al modelo que gestiona el listado de categorias
   listCategory($dbh);                       //Llamada a la funcion listar categorias del modelo anterior y pasamos conexión
--- controller/post_controller.php
+++ controller/post_controller.php
@@ -1,8 +1,9 @@
 <?php
 
 function iniPost($id){
-  require('model/connect_model.php');       //Llamamos al modelo de conexion PDO
-  $dbh = getConection();                    //Realizamos la conexion y la almacenamos en una varible
+  require('./domain/Conecction.php');       //Llamamos al modelo de conexion PDO
+  $conection = new Conecction();
+  $dbh = $conection->getConection();                    //Realizamos la conexion y la almacenamos en una varible
   include('view/post_view.php');            //Llamamos a la vista del listado de temas
   include('model/listpost_model.php');  //LLamamos al modelo que gestiona el listado de temas
   listPost($dbh,$id);                       //Llamada a la funcion listar categorias del modelo anterior y pasamos conexión
