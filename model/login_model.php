352f7c20247f5732e3642b92b47905d517cf2cc0
Juan Senen <juansenen@hotmail.com>
2023-03-18 12:55:19 +0100

    Login css and view

---
+++ model/login_model.php
@@ -0,0 +1,29 @@
+<?php
+function getLogin(){
+  ?>
+  <main class="container" id="content">
+
+<section class="header">
+  <h1>Log in</h1>
+</section>
+  <form method="" action="" class="form-login">
+
+    <div class="form-group">
+      <label for="email">Nombre </label>
+      <input type="text" name="name" id="nombre" value="" class="form-control">
+      <div class="errors"></div>
+    </div>
+
+    <div class="form-group">
+      <label for="password">Password </label>
+      <input type="password" name="password" id="password" class="form-control">
+      <div class="errors"></div>
+    </div>
+
+    <input type="submit" class="btn btn-primary" value="Log in"><br>
+    <p><a href="#">Nuevo Registro</a></p>
+  </form>
+  </main>
+  <?php
+}
+?>
