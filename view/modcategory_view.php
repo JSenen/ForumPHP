22d6eb7721bca3d573c146e0e6f26014e106a0d8
Juan Senen <juansenen@hotmail.com>
2023-03-23 00:44:31 +0100

    Modificar categoria (pendiente)

--- view/modcategory_view.php
+++ view/modcategory_view.php
@@ -1,19 +1,7 @@
-<main class="container admin" id="content">
-    <form action="" method="post" class="narrow">
-      <h1>Modificar Categoria</h1>
+
     
 
-      <div class="form-group">
-        <label for="name">Categoria: </label>
-        <input type="text" name="cat_name" id="cat_name" class="form-control">
-      </div>
+     
 
-      <div class="form-group">
-        <label for="description">Descripción: </label>
-        <textarea name="cat_description" id="cat_description"
-                  class="form-control"></textarea>
-      </div>
 
-      <input type="submit" value="Grabar" name="addcategory"class="btn btn-primary btn-save">
-    </form>
-  </main>
+  
