38cd8f03d3962bf8639ef0480602b5e76d4d2a3a
Juan Senen <juansenen@hotmail.com>
2023-03-22 22:37:57 +0100

    Add Post OK

--- controller/post_controller.php
+++ controller/post_controller.php
@@ -1,10 +1,20 @@
 <?php
-
+require('./domain/Conecction.php');     //Llamamos al modelo de conexion PDO
 function iniPost($id){
-  require('./domain/Conecction.php');     //Llamamos al modelo de conexion PDO
   $conection = new Conecction();
   $dbh = $conection->getConection();      //Realizamos la conexion y la almacenamos en una varible
-  include('view/post_view.php');          //Llamamos a la vista del listado de temas
+  include ('view/header_view.php');              //Llamamos a la cabecera
   include('model/listpost_model.php');    //LLamamos al modelo que gestiona el listado de temas
   listPost($dbh,$id);                     //Llamada a la funcion listar categorias del modelo anterior y pasamos conexión
 }
+
+function addPost($id){
+  $conection = new Conecction();
+  $dbh = $conection->getConection();      //Realizamos la conexion y la almacenamos en una varible
+  include('view/MemberHome_view.php');    //Llamamos a la vista de los usuarios registrado
+  include('view/addpost_view.php');     //LLamamos a la vista para añadir comentarios
+  require('model/addpost_model.php');    //Llamamos al modelo añadir nuevo tema
+  addNewPost($dbh,$id);                  //Llamada a la funcion añadir temas. 
+
+}
+?>
